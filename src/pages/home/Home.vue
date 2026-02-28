fd05f98ae6f45f1cec4ee99b650e6ad445d93bec
Álex Pérez <aperhid@upv.edu.es>
2026-02-28 17:18:27 +0100

    estructura de info y contacto

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
 import { ArrowUp } from 'lucide-vue-next'
 
-import Button from '@/components/ui/button/Button.vue';
-
 import {
   Card,
   CardContent,
@@ -16,6 +14,30 @@ import {
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+
+import { Button } from '@/components/ui/button'
+
+import { Calendar } from '@/components/ui/calendar'
+
+import type { DateValue } from 'reka-ui'
+import { ref } from 'vue'
+
 const scrollToSection = (sectionId: string) => {
     if (sectionId === '#') {
     window.scrollTo({top: 0, behavior:'smooth'})
@@ -26,6 +48,8 @@ const scrollToSection = (sectionId: string) => {
         elemento.scrollIntoView({ behavior:'smooth', block:'start' })
     }
 }
+
+const fecha = ref<DateValue>()
 
 </script>
 
@@ -39,7 +63,7 @@ const scrollToSection = (sectionId: string) => {
                     <NavigationMenuList class="flex flex-col ml-5 sm:flex-row">
                         
                     <NavigationMenuItem>
-                        <a href="#" @click.prevent="scrollToSection('#porfolio')">
+                        <a href="#" @click.prevent="scrollToSection('#')">
                             <NavigationMenuLink :class="[navigationMenuTriggerStyle(), 'text-white bg-transparent hover:text-white hover:scale-105 hover:bg-transparent hover:underline transition-all']">
                              Inicio   
                             </NavigationMenuLink>
@@ -156,12 +180,147 @@ const scrollToSection = (sectionId: string) => {
         <section id="información">
             <div class="información-fondo">
 
+                <div class="card_info">
+                    <Card class="w-80 h-150 border border-[rgba(255,255,255,0.4)] bg-[rgba(255,255,255,0.15)] backdrop-blur-md">
+                        <CardContent>
+                            <h1>dibujo o foto mía</h1>      
+                        </CardContent>
+                    </Card>
+                    
+                    <div class="flex flex-col gap-5">
+                        <Card class="w-150 h-75 border border-[rgba(255,255,255,0.4)] bg-[rgba(255,255,255,0.15)] backdrop-blur-md">
+                            <CardContent>
+                                <h1>estudios</h1>      
+                            </CardContent>
+                        </Card>
+
+                        <Card class="w-150 h-70 border  border-[rgba(255,255,255,0.4)] bg-[rgba(255,255,255,0.15)] backdrop-blur-md">
+                            <CardContent>
+                                <h1>skills</h1>      
+                            </CardContent>
+                        </Card>
+                    </div>
+
+                </div>
+
             </div>
             
         </section>
 
         <section id="contacto">
             <div class="contacto-fondo">
+
+                <div>
+                    <h2>contáctame</h2>
+                </div>
+
+                <div class="card_contacto">
+                    <Card class="border border-[rgba(255,255,255,0.4)] bg-[rgba(255,255,255,0.15)] backdrop-blur-md">
+                        <CardContent>
+                            <div class="w-full">
+                                <iframe 
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.362756509978!2d-0.3466072242107245!3d39.48372267160471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd60488259073375%3A0xfb410ba707ca33c9!2sUPV%20-%20Facultad%20de%20Bellas%20Artes%20BBAA!5e0!3m2!1ses!2ses!4v1771162810587!5m2!1ses!2ses" 
+                                    width="600" 
+                                    height="450" 
+                                    class="w-full h-full rounded-lg"
+                                    allowfullscreen
+                                    loading="lazy" 
+                                    referrerpolicy="no-referrer-when-downgrade"
+                                ></iframe>
+                            </div>     
+                        </CardContent>
+                    </Card>
+
+            
+                    <div>
+                        <Card class="border border-[rgba(255,255,255,0.4)] bg-[rgba(255,255,255,0.15)] backdrop-blur-md">
+                            <CardContent>
+                               <div>
+                                    <form>
+                                        <div class="space-y-2">
+
+                                            <Label for="nombre">Nombre</Label>
+                                            <Input id="nombre" required />
+
+                                        </div>
+                                                
+                                        <div class="space-y-2">
+                                                
+                                            <Label for="apellido">Apellidos</Label>
+                                            <Input id="apellido" required />
+
+                                        </div>
+
+                                        <div class="space-y-2">
+
+                                            <Select required >
+                                                <SelectTrigger class="border-gray-200 bg-white text-gray-900">
+                                                    <SelectValue placeholder="Selecciona una misión"/>
+                                                </SelectTrigger>
+
+                                                <SelectContent class="bg-white">
+                                                    <SelectItem value="rescatare">
+                                                        Rescate de rehenes
+                                                    </SelectItem>
+                                                    <SelectItem value="investigare">
+                                                        Investigación criminal
+                                                    </SelectItem>
+                                                    <SelectItem value="persequi">
+                                                        Persecución de villanos
+                                                    </SelectItem>
+                                                    <SelectItem value="defendere">
+                                                        Defensa de Gotham
+                                                    </SelectItem>
+                                                </SelectContent>
+                                            </Select>
+
+                                        </div>
+
+                                        <div class="space-y-2">
+
+                                            <label>Fecha de la misión</label>
+                                                <Popover required>
+                                                    <PopoverTrigger asChild class="flex"> 
+                                                        <Button variant="outline"> 
+                                                            <span v-if="fecha">
+                                                                {{ fecha.day }}/{{ fecha.month }}/{{ fecha.year }}
+                                                            </span>
+                                                            <span v-else>
+                                                                Selecciona una fecha.
+                                                            </span>
+                                                        </Button>
+                                                    </PopoverTrigger>
+                                                    <PopoverContent>
+                                                        <Calendar v-model="fecha"/>
+                                                    </PopoverContent>
+                                                </Popover>
+                                        </div>
+
+                                        <Button
+                                            type="submit"
+                                            class="w-full bg-[rgb(106,90,205)] hover:bg-[rgb(88,75,171)] text-white text-md mt-4">
+                                                Enviar solicitud
+                                        </Button>
+
+                                    </form>
+                               </div>
+                            </CardContent>
+                        </Card>
+
+                        <div>
+                            <Card class="border border-[rgba(255,255,255,0.4)] bg-[rgba(255,255,255,0.15)] backdrop-blur-md">
+                                <CardContent>
+                                    <h2>redes</h2>
+                                </CardContent>
+                            </Card>     
+                        </div>
+
+                       
+                    </div>
+
+                    
+                    
+                </div>
 
             </div>
             
@@ -301,11 +460,23 @@ const scrollToSection = (sectionId: string) => {
         align-items: center;
         height: 40rem;
         gap: 10rem;
-        
     }
 
-    .contenidoCard {
-        
+    .card_info {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50rem;
+        gap: 2rem;
     }
+
+    .card_contacto {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50rem;
+        gap: 2rem;
+    }
+
   
 </style>
